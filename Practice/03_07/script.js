4ddf8671ed692fd6078bb90af2164c165a4cf155
Karan Gupta <d22124440@mytudublin.ie>
2023-01-23 21:06:46 +0000

    object practice

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,23 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+const waterBottle = {
+  color: "blue",
+  size: "small",
+  shape: "round",
+  empty: false,
+  temperature: 20,
+  labelNames: ["embed", "sticker"],
+  changeTemp: function (newTemp) {
+    return (this.temperature = newTemp);
+  },
+  liquid: {
+    name: "water",
+    composition: ["hydrogen", "oxygen"],
+    opacity: 0,
+    addJuice: function (juiceAmount) {
+      return (this.opacity += juiceAmount);
+    },
+  },
+  weight: "heavy",
+};
